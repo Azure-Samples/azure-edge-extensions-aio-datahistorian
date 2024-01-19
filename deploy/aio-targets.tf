90bd77b94cbac2cc33b00be03f4b736511b3335a
Allen Greaves <allengreaves@microsoft.com>
2024-01-19 10:50:02 -0800

    add telegraf, configuration, and aio targets

--- deploy/aio-targets.tf
+++ deploy/aio-targets.tf
@@ -1,5 +1,6 @@
 locals {
-  volume_mounts = yamldecode(file("./manifests/volumes-mount.tftpl.yaml"))
+  volume_mounts      = yamldecode(file("./manifests/volumes-mount.tftpl.yaml"))
+  telegraf_conf_file = file("./manifests/telegraf/telegraf.tftpl.conf")
 }
 
 resource "azapi_resource" "aio_targets_data_historian" {
@@ -114,6 +115,67 @@ resource "azapi_resource" "aio_targets_http_echo" {
           type = "yaml.k8s"
           properties = {
             resource = yamldecode(file("./manifests/echo/echo-service.tftpl.yaml"))
+          }
+        },
+      ]
+
+      topologies = [
+        {
+          bindings = [
+            {
+              role     = "yaml.k8s"
+              provider = "providers.target.kubectl"
+              config = {
+                inCluster = "true"
+              }
+            }
+          ]
+        }
+      ]
+    }
+  })
+}
+
+resource "azapi_resource" "aio_targets_telegraf" {
+  schema_validation_enabled = false
+  type                      = "Microsoft.IoTOperationsOrchestrator/Targets@2023-10-04-preview"
+  name                      = "${var.name}-tgt-tg"
+  location                  = var.location
+  parent_id                 = data.azurerm_resource_group.this.id
+
+  body = jsonencode({
+    extendedLocation = {
+      name = local.custom_locations_id
+      type = "CustomLocation"
+    }
+
+    properties = {
+      scope   = var.aio_cluster_namespace
+      version = var.datahistorian_targets_main_version
+      components = [
+        {
+          name = "telegraf-deployment"
+          type = "yaml.k8s"
+          properties = {
+            resource = yamldecode(templatefile("./manifests/telegraf/telegraf-deployment.tftpl.yaml", {
+              telegraf_conf_hash = sha256(local.telegraf_conf_file)
+            }))
+          }
+        },
+        {
+          name = "telegraf-service"
+          type = "yaml.k8s"
+          properties = {
+            resource = yamldecode(file("./manifests/telegraf/telegraf-service.tftpl.yaml"))
+          }
+        },
+        {
+          name = "telegraf-configmap"
+          type = "yaml.k8s"
+          properties = {
+            resource = yamldecode(templatefile("./manifests/telegraf/telegraf-configmap.tftpl.yaml", {
+              telegraf_conf = join("\n", [for line in split("\n", local.telegraf_conf_file) : "    ${line}"])
+            }))
           }
         },
       ]
